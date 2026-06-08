dd9527feefed5f3b3d3567dee22e93694ba6edd7
DonQuaan <donquaan@example.com>
2026-06-08 16:58:44 +0700

    chore: setup release scripts and app version display

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log(
+  `%c DonQuaan Profile %c v${import.meta.env.VITE_APP_VERSION} `,
+  'background: #000; color: #fff; border-radius: 3px 0 0 3px; padding: 4px; font-weight: bold;',
+  'background: #333; color: #fff; border-radius: 0 3px 3px 0; padding: 4px; font-weight: bold;'
+)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
